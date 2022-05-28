b2a5b223673188d10d506ca509d1f7026c58ac9a
gpseal <sealgp1@student.op.ac.nz>
2022-05-29 11:49:08 +1200

    feat: add initial navigation menu

--- sealgp1-react/src/App.js
+++ sealgp1-react/src/App.js
@@ -1,37 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import axios from "axios";
-import { useEffect, useState } from "react"; // Import the useEffect hook from the react dependency
+import Navigation from "./components/Navigation";
+import InstitutionsTable from "./components/tables/InstitutionsTable";
 
-
-const Post = () => {
-  const [post, setPost] = useState([]); // State variables
-
-  const getPosts = async () => {
-    try {
-      const response = await axios.get(
-        "https://jsonplaceholder.typicode.com/posts/1"
-      );
-      setPost(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    getPosts();
-  }, []); /*
-           The empty array means render once. If we pass in post, i.e., [post],
-           it will re-render the component if post's data changes
-         */
-
+const App = () => {
   return (
-    <div>
-      {/* Rendering the post's title and body */}
-      <h1>{post.title}</h1>
-      <p>{post.body}</p>
-    </div>
-  );
+  <div>
+  <Navigation />
+  <InstitutionsTable />
+  </div>
+
+  )
 };
 
-export default Post;
+export default App;
---
+++ sealgp1-react/src/components/Navigation.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from "reactstrap";
+
+const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <Navbar color="dark" dark expand="md">
+      <NavbarBrand href="/">Student Management System</NavbarBrand>
+      <NavbarToggler onClick={toggle} />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className="ms-auto" navbar>
+          <NavItem>
+            <NavLink href="/login">Login</NavLink>
+          </NavItem>
+        </Nav>
+      </Collapse>
+    </Navbar>
+  );
+};
+
+export default Navigation;
