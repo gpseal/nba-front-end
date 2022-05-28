d501d94caa6d5b6b8959b171d01feac58a1a1d74
gpseal <sealgp1@student.op.ac.nz>
2022-05-29 11:56:32 +1200

    feat: add table, access through navigation bar

--- sealgp1-react/src/components/Navigation.js
+++ sealgp1-react/src/components/Navigation.js
@@ -1,4 +1,8 @@
 import { useState } from "react";
+
+// Import the following:
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import {
   Collapse,
   Navbar,
@@ -9,23 +13,35 @@ import {
   NavLink,
 } from "reactstrap";
 
+// Import the following component:
+import InstitutionsTable from "./tables/InstitutionsTable";
+
 const Navigation = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => setIsOpen(!isOpen);
 
   return (
-    <Navbar color="dark" dark expand="md">
-      <NavbarBrand href="/">Student Management System</NavbarBrand>
-      <NavbarToggler onClick={toggle} />
-      <Collapse isOpen={isOpen} navbar>
-        <Nav className="ms-auto" navbar>
-          <NavItem>
-            <NavLink href="/login">Login</NavLink>
-          </NavItem>
-        </Nav>
-      </Collapse>
-    </Navbar>
+    <Router>
+      <Navbar color="dark" dark expand="md">
+        <NavbarBrand href="/">Student Management System</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ms-auto" navbar>
+            <NavItem>
+              <NavLink href="/login">Login</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/institutions">Institutions</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+      <Routes>
+        <Route path="/login" />
+        <Route path="/institutions" element={<InstitutionsTable />} />
+      </Routes>
+    </Router>
   );
 };
 
