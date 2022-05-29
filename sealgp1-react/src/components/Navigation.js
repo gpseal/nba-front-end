1021be00c6fd98b05eb484e0b4b35362c4ae9fc0
gpseal <sealgp1@student.op.ac.nz>
2022-05-29 15:04:24 +1200

    feat: add site login

--- sealgp1-react/src/components/Navigation.js
+++ sealgp1-react/src/components/Navigation.js
@@ -1,10 +1,9 @@
-import { useState } from "react";
-
-// Import the following:
+import axios from "axios";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import {
   Collapse,
+  Container,
   Navbar,
   NavbarToggler,
   NavbarBrand,
@@ -12,14 +11,64 @@ import {
   NavItem,
   NavLink,
 } from "reactstrap";
-
-// Import the following component:
+import LoginForm from "./forms/LoginForm";
 import InstitutionsTable from "./tables/InstitutionsTable";
 
 const Navigation = () => {
+    const BASE_URL = "https://id607001-sealgp1.herokuapp.com";
+
   const [isOpen, setIsOpen] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(
+    sessionStorage.getItem("isLoggedIn") === "true" || false
+  );
 
   const toggle = () => setIsOpen(!isOpen);
+
+  const login = () => {
+    setIsLoggedIn(true);
+    sessionStorage.setItem("isLoggedIn", true);
+    alert("Logged in."); // Debugging purposes
+  };
+
+  const logout = async () => {
+    try {      
+      const res = await axios.post(`${BASE_URL}/api/v1/logout`, {
+        headers: {
+          'Authorization' : `Bearer ${sessionStorage.getItem("token")}`
+        }
+      });
+
+      if (res.status === 200) {
+        setIsLoggedIn(false);
+        sessionStorage.clear();
+        alert("Logged out."); // Debugging purposes
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Render a NavLink based on whether a user is logged in or out
+  const authLink = isLoggedIn ? (
+    <>
+      <NavItem>
+        <NavLink href="/institutions">Institutions</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink onClick={logout} style={{ cursor: "pointer" }}>
+          Logout
+        </NavLink>
+      </NavItem>
+    </>
+  ) : (
+    <NavLink href="/login">Login</NavLink>
+  );
+
+  const authResources = isLoggedIn ? (
+    <Route path="/institutions" element={<InstitutionsTable />} />
+  ) : (
+    <></>
+  );
 
   return (
     <Router>
@@ -28,19 +77,16 @@ const Navigation = () => {
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="ms-auto" navbar>
-            <NavItem>
-              <NavLink href="/login">Login</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/institutions">Institutions</NavLink>
-            </NavItem>
+            {authLink}
           </Nav>
         </Collapse>
       </Navbar>
-      <Routes>
-        <Route path="/login" />
-        <Route path="/institutions" element={<InstitutionsTable />} />
-      </Routes>
+      <Container>
+        <Routes>
+          <Route path="/login" element={<LoginForm login={login} />} />
+          {authResources}
+        </Routes>
+      </Container>
     </Router>
   );
 };
